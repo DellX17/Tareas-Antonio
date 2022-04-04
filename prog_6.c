0289cf485d7439a1fc0d10f5ef11fc1acd053d33
DellX17 <101080938+DellX17@users.noreply.github.com>
2022-04-03 22:49:35 -0500

    Create prog_6.c

---
+++ prog_6.c
@@ -0,0 +1,35 @@
+//
+//  Aarón Aguilar
+//  
+//  Media y varianza
+//  
+//  03.04.2022
+#include <stdio.h>
+
+int main()
+{
+    int compara(char *cad){
+char password[]="calor";
+int i,s;
+s=0;
+for(i=0; i<5; i++){
+s = s + cad[i]-password[i];
+}
+return s;
+}
+int tam = 5;
+int tmp = 0;
+int s = 1;
+char pass[5];
+while (s!=0){
+for (int i = 0; i < tam; i++)
+    {
+      tmp = rand() % 78;
+      pass[i] = tmp+'0';
+    }
+    
+    s = compara(pass);
+}
+printf("Contraseña: %s", pass);
+return 0;
+}
